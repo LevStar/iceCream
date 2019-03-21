cae93f6810a925acd4b7a39ae306ddbee4902c96
LevStar <lev.fruchter@uamaker.nyc>
2019-03-20 21:21:44 -0400

    rough cone

--- iceCream.py
+++ iceCream.py
@@ -8,6 +8,12 @@ def draw_circ(cX, cY, cRad, cCol, cWin):
     circ.setOutline(cCol)
     circ.draw(cWin)
 
+def draw_tri(tX1, tY1, tX2, tY2, tX3, tY3, tCol, tWin):
+    tri = Polygon(Point(tX1, tY1), Point(tX2, tY2), Point(tX3, tY3))
+    tri.setFill(tCol)
+    tri.setOutline(tCol)
+    tri.draw(tWin)
+
 icRad = 50
 winFactor = 10
 winX = icRad * winFactor
@@ -17,4 +23,9 @@ icWin = GraphWin("Have a cone!", winX, winY)
 icWin.setCoords(0, 0, winX, winY)
 
 draw_circ(winX / 2, winY / 2 + icRad, icRad, "pink", icWin)
-
+draw_tri(winX / 2 - icRad - 2, winY / 2 + icRad * .75,
+         winX / 2 + icRad + 2, winY / 2 + icRad * .75,
+         winX / 2 , winY / 2 - icRad * 2,
+         "orange", icWin)
+         
+         
