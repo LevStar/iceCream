299115032ba54698bcbc1483059f263f09c386e6
LevStar <lev.fruchter@uamaker.nyc>
2019-03-20 21:08:01 -0400

    rough scoop

---
+++ iceCream.py
@@ -0,0 +1,20 @@
+# iceCream by LevStar and LevStar
+
+from graphics import *
+
+def draw_circ(cX, cY, cRad, cCol, cWin):
+    circ = Circle(Point(cX, cY), cRad)
+    circ.setFill(cCol)
+    circ.setOutline(cCol)
+    circ.draw(cWin)
+
+icRad = 50
+winFactor = 10
+winX = icRad * winFactor
+winY = icRad * winFactor
+
+icWin = GraphWin("Have a cone!", winX, winY)
+icWin.setFill(0, 0, winX, winY)
+
+draw_circ(winX / 2, winY / 2 + icRad * 2, icRad, "pink", icWin)
+
