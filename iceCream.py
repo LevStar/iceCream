3b7c2e8081d453da5d8ed54ca20866703e55b2d8
LevStar <lev.fruchter@uamaker.nyc>
2019-03-28 14:43:52 -0400

    starting over with no closing

--- iceCream.py
+++ iceCream.py
@@ -44,7 +44,3 @@ icWin.setCoords(0, 0, winX, winY)
 draw_circ(winX / 2, winY / 2 + icRad, icRad, "pink", icWin)
 draw_cone(winX, winY, icRad, "orange", icWin)
 
-# Added closing scripts.
-icWin.getMouse()
-icWin.close()
-
